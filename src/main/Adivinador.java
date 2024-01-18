28a829c388016c94692ce5528b7a06b2928a45ba
Esteban-02 <OESTEBAN02@GMAIL.COM>
2024-01-18 16:54:32 -0500

    Resutados Juego

--- src/main/Adivinador.java
+++ src/main/Adivinador.java
@@ -17,7 +17,7 @@ public class Adivinador {
      * Las dificuldes disponibles en el juego
      * <ul>
      *     <li><b> JUGANDO: </b> El juego ha iniciado. No se ha ganado aun, ni se ha perdido.</li>
-     *     <li><b> GANIO:</b> El jugador adivino el numero secreto antes de agotar los intentos.</li>
+     *     <li><b> GANIO:</b> El jugad qor adivino el numero secreto antes de agotar los intentos.</li>
      *     <li><b> PERDIO: </b> EL jugador agoto los intentos antes de adivinar el numero secreto.</li>
      * </ul>
      */
@@ -30,6 +30,16 @@ public class Adivinador {
      */
     public static enum Resultado {ES_MAYOR, ES_IGUAL, ES_MENOR}
 
+    /**
+     * Modelo de juego: <br>
+     * <ul><li><b>Dificultad:</b> Arreglo de una celda conteniendo la dificultad del juego</li>
+     * <li><b>maxIntentos:</b> Cantidad maxima de intentos con que se inicio el juego segun su dificultad.</li>
+     * <li><b>estado:</b> Arreglo de la celda conteniendo el estado actual del juego.</li>
+     * <li><b>numSecreto:</b> El numero secreto generado aleatoriamente entre el 1 y el NUMERO_MAXIMO.</li>
+     * <li><b>intentoActual:</b> El intento actual del jugador</li>
+     * </ul>
+     */
+
     public static record Juego (
             Dificultad[] dificultad,
             AtomicInteger maxIntentos,
@@ -38,10 +48,38 @@ public class Adivinador {
             AtomiInteger intentoActal,
     ){}
 
+    /**
+     * Inicializa el juego con los valores correctos acorde a la dificultad asigada.
+     * El estado inicial sera JUGANDO y el intento actual sera 0. Tambien se generara
+     * un numero aleatorio entre 1 y NUMERO_MAXIMO
+     * @param d La dificutad con que se iniciara el juego
+     * @return Retorna un juego listo para comenzar
+     * @see Dificultad
+     * @see Estado
+     * @see Juego
+     * @see NUMERO_MAXIMO
+     */
 
+    public static Juego iniciarJuego(Dificultad d){
+        Juego j = new Juego(new Dificultad[1], new AtomicInteger(), Estado[1], new AtomicInteger(), new AtomicInteger());
+        j.dificultad[0]=d;
+        if (d==Dificultad.FACIL){
+            j.maxIntentos.set(MAX_INTENTOS_FACIL);
+        }else {
+            j.maxIntentos.set(MAX_INTENTOS_DIFICIL);
+        }
 
+        j.estado[0]=Estado.JUGANDO;
+        j.numeroSecreto.set(new Random().nextInt(NUMERO_MAXIMO)+1;
 
+        return j;
 }
+
+    public static Resultado verificarNumero(int n, Juego j, StringBuilder mensaje){
+        j.intentoActal.set(j.intentoActal.get()+1);
+        mensaje.d
+    }
+
 
 
     /**
